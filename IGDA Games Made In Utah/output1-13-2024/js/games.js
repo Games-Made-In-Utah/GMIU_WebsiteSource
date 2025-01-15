3d1d210853d1562413df57a34a94c794fcec3be5
lstevensN <lstevens@student.neumont.edu>
2025-01-15 12:15:26 -0700

    Project Restructuring
    Simplified output folder and organized header & footer.

---
+++ IGDA Games Made In Utah/output1-13-2024/js/games.js
@@ -0,0 +1,25 @@
+﻿// Credits to https://www.geeksforgeeks.org/read-json-file-using-javascript/
+// Thanks for the help!
+
+let games = null;
+
+function setGames(data) { games = data; console.log(games); }
+
+function fetchGames() {
+    fetch("/data/games.json")
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error
+                    (`HTTP error! Status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((data) => {
+            setGames(data);
+            console.log(data);
+        })
+        .catch((error) =>
+            console.error("Unable to fetch data:", error));
+}
+
+fetchGames();
---
+++ IGDA Games Made In Utah/output1-13-2024/js/site.js
@@ -0,0 +1,9 @@
+﻿// Credits to https://stackoverflow.com/questions/32938168/using-innerhtml-property-to-display-content-of-another-html-file
+// HUGE thanks for the assist!
+
+$(function () {
+    $("#header").load("header.html");  // Loads page header
+    $("#footer").load("footer.html");  // Loads page footer
+
+    $('body').addClass('fade-in');  // Smooth fade-in on page load
+});
